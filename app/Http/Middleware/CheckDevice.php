d6bdb39f717b8e902f7dc9b59d986be12f62eaaa
Rafy Aulia Akbar <rafy683@gmail.com>
2017-08-30 21:02:16 +0700

    entah

--- app/Http/Middleware/CheckDevice.php
+++ app/Http/Middleware/CheckDevice.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Jenssegers\Agent\Agent as Agent;
 
 class CheckDevice
 {
@@ -15,6 +16,9 @@ class CheckDevice
      */
     public function handle($request, Closure $next)
     {
+        if ((new Agent())->isDesktop()){
+            return redirect('sorry');
+        }
         return $next($request);
     }
 }
--- resources/views/sorry.blade.php
+++ resources/views/sorry.blade.php
@@ -1,7 +1,1 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Rafy
- * Date: 30/08/2017
- * Time: 19.57
- */
+Maaf, aplikasi ini hanya tersedia untuk web
